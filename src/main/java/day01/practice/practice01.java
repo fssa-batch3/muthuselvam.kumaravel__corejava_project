299b4763e2ac5ccdb1bb162d8c7e46fd0c46e075
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-07-10 22:06:17 +0530

    commit changes

---
+++ src/main/java/day01/practice/practice01.java
@@ -0,0 +1,10 @@
+package day01.practice;
+
+public class practice01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/day01/solved/package01/Dog.java
@@ -0,0 +1,10 @@
+package day01.solved.package01;
+
+public class Dog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
