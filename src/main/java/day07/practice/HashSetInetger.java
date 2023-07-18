04fd0cd5edce0ab0488841d27a8809842d510b52
Muthuselvam-freshworks <116705334+Muthuselvam-freshworks@users.noreply.github.com>
2023-07-18 18:08:02 +0530

    commit changes

---
+++ src/main/java/day07/practice/HashSetInetger.java
@@ -0,0 +1,32 @@
+package day07.practice;
+
+
+import java.util.*;
+
+public class HashSetInetger {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> cityNames = new ArrayList<Integer>();
+		cityNames.add(1);
+		cityNames.add(2);
+		cityNames.add(2);
+		cityNames.add(3);
+
+		System.out.println("Using ArrayList :" + cityNames);
+
+		// Set stores unique elements and ignores duplication
+		HashSet<Integer> cityNamesSet = new HashSet<Integer>();
+		cityNamesSet.add(1);
+		cityNamesSet.add(2);
+		cityNamesSet.add(2);
+		cityNamesSet.add(3);
+
+		System.out.println("Using Set :" + cityNamesSet);
+		for (int cityName : cityNamesSet) {
+			System.out.println(cityName);
+		}
+		
+
+	}
+}
